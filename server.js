8dea08b1babb90398ae22213309d1d456cc92bc5
allen wong <allencwwong@verizonmedia.com>
2019-12-27 00:00:40 -0800

    created server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let morgan = require('morgan');
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
